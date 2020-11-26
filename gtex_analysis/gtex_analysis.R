795227d3841b064ca0a8ac16b4e55513ae52dbba
j-mitchel <jonathan.mitchel3@gmail.com>
2020-11-25 21:46:49 -0500

    edited scripts, need testing still

--- gtex_analysis/gtex_analysis.R
+++ gtex_analysis/gtex_analysis.R
@@ -1,52 +1,82 @@
 
+library(scITD)
+
 # read in the processed data
 gtex_tpm_sub_transform <- readRDS(file='/home/jmitchel/data/gtex/7_tissues_counts.rds')
 gtex_meta <- readRDS(file='/home/jmitchel/data/gtex/7_tissues_meta.rds')
 feature.names.final <- readRDS(file='/home/jmitchel/data/gtex/genes.rds')
 
 
-# basic scITD pipeline
-ctypes_use <- unique(gtex_meta$ctypes)
-pbmc_scMinimal <- instantiate_scMinimal(data_sparse=gtex_tpm_sub_transform,
+## basic scITD pipeline with some slight adjustments for the bulk RNA data
+ttypes_use <- unique(gtex_meta$ctypes)
+gtex_scMinimal <- instantiate_scMinimal(data_sparse=gtex_tpm_sub_transform,
                                         meta_data=gtex_meta)
-pbmc_container <- make_new_container(pbmc_scMinimal,
-                                     ctypes_use = ctypes_use,
+gtex_container <- make_new_container(gtex_scMinimal,
+                                     ctypes_use = ttypes_use,
                                      gn_convert = feature.names.final,
                                      scale_var = TRUE,
-                                     var_scale_power = 1, rotate_modes = 'donors',
+                                     var_scale_power = 1,
+                                     tucker_type = 'sparse', 
+                                     rotation_type = 'ica',
                                      ncores = 30, rand_seed = 10)
 
-pbmc_container <- get_ctype_data(pbmc_container,donor_min_cells=0)
+# setting donor_min_cells to 0 because in this case there is only one sample per
+# donor per tissue type, whereas usually, there are several cells per donor per cell type
+gtex_container <- get_ctype_data(gtex_container,donor_min_cells=0)
 
-pbmc_container <- get_ctype_vargenes(pbmc_container, method="norm_var", thresh=1000)
+# using norm_var method because the anova and empir methods only work with single cell data
+gtex_container <- get_ctype_vargenes(gtex_container, method="norm_var", thresh=500)
 
-pbmc_container <- run_tucker_ica(pbmc_container, ranks=c(10,20,7), shuffle=FALSE)
-
-pbmc_container <- plot_donor_matrix(pbmc_container, meta_vars=c('sex'),
-                                    cluster_by_meta='sex')
-pbmc_container$plots$donor_matrix
-
-pbmc_container <- optimize_var_scale_power(pbmc_container,min_ranks_test=c(2,10,7),
+# get variance scaling power parameter
+gtex_container <- optimize_var_scale_power(gtex_container,min_ranks_test=c(2,10,7),
                                            max_ranks_test=c(7,15,7),
                                            min_power_test=0.25,max_power_test=1)
-pbmc_container$plots$var_scale_plot
+gtex_container$plots$var_scale_plot
 
-pbmc_container <- set_experiment_params(pbmc_container, var_scale_power = 1)
+# adjust var scale param as needed
+gtex_container <- set_experiment_params(gtex_container, var_scale_power = 1)
 
-pbmc_container <- determine_ranks_tucker(pbmc_container, max_ranks_test=c(12,20,7),
+# determine appropriate ranks
+gtex_container <- determine_ranks_tucker(gtex_container, max_ranks_test=c(12,20,7),
                                          method='svd', shuffle_level='tensor', num_iter=5)
-pbmc_container$plots$rank_determination_plot
+gtex_container$plots$rank_determination_plot
 
-pbmc_container <- run_tucker_ica(pbmc_container, ranks=c(9,20,7), shuffle=FALSE)
+# run tucker
+gtex_container <- run_tucker_ica(gtex_container, ranks=c(10,20,7), shuffle=FALSE)
 
-pbmc_container <- plot_donor_matrix(pbmc_container, meta_vars=c('sex'),
-                                    cluster_by_meta='sex', show_donor_ids=FALSE)
-pbmc_container$plots$donor_matrix
+# plot donor scores
+gtex_container <- plot_donor_matrix(gtex_container, meta_vars=c('sex'),
+                                    cluster_by_meta='sex')
+gtex_container$plots$donor_matrix
 
-pbmc_container <- plot_loadings_annot(pbmc_container, factor_select=9,
-                                      use_sig_only=F, annot='none', display_genes=F)
-pbmc_container$plots$single_lds_plot
+# get significant genes
+gtex_container <- run_jackstraw(gtex_container, n_fibers=100, n_iter=500)
 
-pbmc_container <- run_gsea_one_factor(pbmc_container, factor_select=9, method="fgsea",
+
+# check that significant genes correspond with large magnitude loadings
+gtex_container <- get_all_lds_factor_plots(gtex_container, use_sig_only=FALSE, 
+                                           nonsig_to_zero=FALSE, 
+                                           annot='sig_genes',
+                                           sig_thresh=0.05, 
+                                           display_genes=FALSE,
+                                           gene_callouts=FALSE)
+
+render_all_lds_plots(gtex_container, n_rows=3)
+
+
+# plot loadings of significant genes only and add gene callouts
+gtex_container <- get_all_lds_factor_plots(gtex_container, use_sig_only=TRUE, 
+                                           nonsig_to_zero=TRUE, 
+                                           annot='sig_genes',
+                                           sig_thresh=0.05, 
+                                           display_genes=FALSE,
+                                           gene_callouts=TRUE)
+
+render_all_lds_plots(gtex_container, n_rows=3)
+
+# run gsea for an interesting factor
+gtex_container <- run_gsea_one_factor(gtex_container, factor_select=9, method="fgsea",
                                       thresh=0.005, db_use="GO", num_iter=10000)
-pbmc_container$plots$gsea[['Factor2']][['up']]
+gtex_container$plots$gsea[['Factor2']][['up']]
+
+
---
+++ vdw_pbmc_analysis/pbmc_analysis.R
@@ -0,0 +1,120 @@
+
+library(scITD)
+
+# counts matrix
+pbmc_counts <- readRDS('/home/jmitchel/data/van_der_wijst/pbmc_counts.rds')
+
+# meta data matrix
+pbmc_meta <- readRDS('/home/jmitchel/data/van_der_wijst/pbmc_meta.rds')
+
+# ensembl to gene name conversions
+feature.names <- readRDS('/home/jmitchel/data/van_der_wijst/genes.rds')
+
+# put data in project container
+pbmc_scMinimal <- instantiate_scMinimal(count_data=pbmc_counts, meta_data=pbmc_meta)
+pbmc_container <- make_new_container(pbmc_scMinimal,
+                                     ctypes_use = c("CD4+ T", "CD8+ T", "cMonocyte", "CD56(dim) NK", "B"),
+                                     gn_convert = feature.names, scale_var = TRUE,
+                                     var_scale_power = 1.25,
+                                     tucker_type = 'sparse', rotation_type = 'ica',
+                                     ncores = 30, rand_seed = 10)
+
+# get sex meta data
+pbmc_container <- identify_sex_metadata(pbmc_container)
+
+# finish prepping the data
+pbmc_container <- get_ctype_data(pbmc_container)
+pbmc_container <- get_ctype_vargenes(pbmc_container, method="empir", thresh=0.01)
+
+# determine appropriate variance scaling parameter
+pbmc_container <- optimize_var_scale_power(pbmc_container, min_ranks_test=c(5,8,5),
+                                           max_ranks_test=c(10,15,5),
+                                           min_power_test=1.5,
+                                           max_power_test=2.25)
+pbmc_container$plots$var_scale_plot
+
+# determine appropriate ranks to use for decomposition
+pbmc_container <- determine_ranks_tucker(pbmc_container, max_ranks_test=c(6,10,5),
+                                         method='svd', num_iter=5, shuffle_level='cells')
+pbmc_container$plots$rank_determination_plot
+
+# run tucker
+pbmc_container <- run_tucker_ica(pbmc_container, ranks=c(5,8,5), shuffle=FALSE)
+
+# plot donor scores
+pbmc_container <- plot_donor_matrix(pbmc_container, meta_vars=c('sex','lanes'),
+                                    cluster_by_meta='sex', show_donor_ids = FALSE)
+pbmc_container$plots$donor_matrix
+
+# get significant genes
+pbmc_container <- run_jackstraw(pbmc_container, n_fibers=100, n_iter=500)
+
+# show that significant genes correspond to large magnitude loadings
+pbmc_container <- get_all_lds_factor_plots(pbmc_container, use_sig_only=FALSE, 
+                                           nonsig_to_zero=FALSE, 
+                                           annot='sig_genes',
+                                           sig_thresh=0.05, 
+                                           display_genes=FALSE,
+                                           gene_callouts=FALSE)
+render_all_lds_plots(pbmc_container, n_rows=2)
+
+# plot loadings with significant genes only and gene callouts
+pbmc_container <- get_all_lds_factor_plots(pbmc_container, use_sig_only=TRUE, 
+                                           nonsig_to_zero=TRUE, annot='none',
+                                           sig_thresh=0.05, display_genes=FALSE,
+                                           gene_callouts=TRUE,
+                                           callout_n_gene_per_ctype=5, 
+                                           callout_ctypes=list(c(NULL),
+                                                               c('CD4+ T',
+                                                                 'cMonocyte'),
+                                                               c(NULL),
+                                                               c('CD8+ T'),
+                                                               c(NULL)))
+render_all_lds_plots(pbmc_container, n_rows=2)
+
+### plot_donor_sig_genes and run_gsea_one_factor should be made automatic for all
+### all factors and resulting plots should be auto formatted...
+# generate some plots of scaled expression for top loadings genes of a factor
+pbmc_container <- plot_donor_sig_genes(pbmc_container, factor_select=2, 
+                                       top_n_per_ctype=c(30,30), 
+                                       ctypes_use=c('CD4+ T','cMonocyte'))
+pbmc_container$plots$donor_sig_genes$Factor2
+
+pbmc_container <- plot_donor_sig_genes(pbmc_container, factor_select=4, 
+                                       top_n_per_ctype=c(5,20), 
+                                       ctypes_use=c('CD4+ T','CD8+ T'))
+pbmc_container$plots$donor_sig_genes$Factor4
+
+
+# run gsea for a few factors
+pbmc_container <- run_gsea_one_factor(pbmc_container, factor_select=2, method="fgsea", thresh=0.01,
+                    db_use="GO", collapse_paths=TRUE)
+pbmc_container$plots$gsea$Factor2
+
+pbmc_container <- run_gsea_one_factor(pbmc_container, factor_select=2, method="fgsea", thresh=0.01,
+                                      db_use="GO", collapse_paths=TRUE)
+pbmc_container$plots$gsea$Factor4
+
+
+# run cell subtype proportion analysis
+pbmc_container <- get_subtype_prop_associations(pbmc_container,max_res=1.5,
+                                                stat_type='adj_pval',
+                                                integration_var='lanes')
+container$plots$subtype_prop_factor_associations
+
+# generate subcluster plots and association significance for individual subtypes
+pbmc_container <- get_all_subclust_plots(pbmc_container,
+                                         ctypes=c('CD4+ T','CD56(dim) NK',
+                                                  'cMonocyte','CD8+ T'),
+                                         res=c(.7,.6,.5,.5),
+                                         factors=c(2,2,2,4))
+
+render_subtype_plots(pbmc_container)
+
+# get associations between proportions of major cell types and factors
+pbmc_container <- get_ctype_prop_associations(pbmc_container,
+                                              stat_type='adj_pval')
+pbmc_container$plots$ctype_prop_factor_associations
+
+
+

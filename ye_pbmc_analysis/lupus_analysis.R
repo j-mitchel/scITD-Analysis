154dc2c7c8291671cefbfda8f7dca41dc476f0d4
Jonathan <jonathan.mitchel3@gmail.com>
2020-12-07 00:18:05 -0500

    updates

--- ye_pbmc_analysis/lupus_analysis.R
+++ ye_pbmc_analysis/lupus_analysis.R
@@ -1,13 +1,11 @@
 
 library(scITD)
+library(edgeR)
 library(Seurat)
 library(SeuratDisk)
 
-# read the h5seurat file
-pbmc <- LoadH5Seurat("/home/jmitchel/data/lupus_data/Lupus_study_adjusted.h5seurat")
-
-# normalize the data
-pbmc <- NormalizeData(pbmc)
+# load the cleaned data
+readRDS()
 
 # prep the container
 pbmc_scMinimal <- seurat_to_scMinimal(pbmc,normalize_counts=FALSE, 
@@ -18,7 +16,8 @@ pbmc_scMinimal <- seurat_to_scMinimal(pbmc,normalize_counts=FALSE,
                                                       "cg_cov",
                                                       "sex",
                                                       "age",
-                                                      "batch_cov"),
+                                                      "batch_cov",
+                                                      "Processing_Cohort"),
                                       metadata_col_nm=c('donors',
                                                         'donor_genotype',
                                                         'SLE_status',
@@ -26,7 +25,8 @@ pbmc_scMinimal <- seurat_to_scMinimal(pbmc,normalize_counts=FALSE,
                                                         'ctypes',
                                                         'sex',
                                                         'age',
-                                                        'pool'))
+                                                        'pool',
+                                                        'processing'))
 
 
 # get median number cells per donor 
@@ -49,6 +49,13 @@ pbmc_container <- make_new_container(pbmc_scMinimal,
                                      tucker_type = 'sparse', rotation_type = 'ica',
                                      ncores = 30, rand_seed = 10)
 
+pbmc_container <- make_new_container(pbmc_scMinimal,
+                                     ctypes_use = c("NK","T4","T8","cM"),
+                                     scale_var = TRUE,
+                                     var_scale_power = 1.5,
+                                     tucker_type = 'sparse', rotation_type = 'ica',
+                                     ncores = 30, rand_seed = 10)
+
 rm(pbmc_scMinimal)
 gc()
 
@@ -56,6 +63,20 @@ gc()
 # finish prepping the data
 pbmc_container <- get_ctype_data(pbmc_container)
 pbmc_container <- get_ctype_vargenes(pbmc_container, method="norm_var", thresh=1000)
+
+# trying mixed model for batch correct
+pbmc_container <- get_ctype_data(pbmc_container)
+pbmc_container <- get_ctype_vargenes(pbmc_container, method="anova", thresh=.05)
+
+
+# testing out batch correction on collapsed data before var gene selection
+pbmc_container <- get_ctype_data(pbmc_container)
+pbmc_container <- collapse_by_donors(pbmc_container, shuffle=FALSE)
+pbmc_container <- apply_pseudobulk_batch_correct(pbmc_container,'pool')
+pbmc_container <- get_ctype_vargenes(pbmc_container, method="norm_var", thresh=1000) #make necessary changes
+## probably need more changes to make it work with run tucker ica because this recalls collapse by donors
+## maybe need to have the apply pseudobulk fn put the data in data sparse. I think I do something similar with
+## the gtex data so take a look at that though that may have been relating to vargenes fn only
 
 # determine appropriate variance scaling parameter
 pbmc_container <- optimize_var_scale_power(pbmc_container, min_ranks_test=c(5,8,5),
@@ -65,16 +86,21 @@ pbmc_container <- optimize_var_scale_power(pbmc_container, min_ranks_test=c(5,8,
 pbmc_container$plots$var_scale_plot
 
 # determine appropriate ranks to use for decomposition
-pbmc_container <- determine_ranks_tucker(pbmc_container, max_ranks_test=c(6,10,5),
+pbmc_container <- determine_ranks_tucker(pbmc_container, max_ranks_test=c(10,18,7),
                                          method='svd', num_iter=5, shuffle_level='cells')
 pbmc_container$plots$rank_determination_plot
 
 # run tucker
-pbmc_container <- run_tucker_ica(pbmc_container, ranks=c(8,16,7), shuffle=FALSE)
+pbmc_container <- run_tucker_ica(pbmc_container, ranks=c(7,10,4), shuffle=FALSE)
+pbmc_container <- run_tucker_ica(pbmc_container, ranks=c(5,8,4), shuffle=FALSE)
+pbmc_container <- run_tucker_ica(pbmc_container, ranks=c(5,10,7), shuffle=FALSE)
+
 
 # plot donor scores first by clustering by sex
 pbmc_container <- plot_donor_matrix(pbmc_container, meta_vars=c('sex','Status','pool'),
-                                    cluster_by_meta='Status', show_donor_ids = FALSE)
+                                    cluster_by_meta='sex', show_donor_ids = FALSE)
+pbmc_container <- plot_donor_matrix(pbmc_container, meta_vars=c('sex','Status','pool'),
+                                    show_donor_ids = FALSE)
 pbmc_container <- plot_donor_matrix(pbmc_container, meta_vars=c('Status','pool'),
                                     cluster_by_meta='pool', show_donor_ids = FALSE)
 pbmc_container$plots$donor_matrix
@@ -83,6 +109,9 @@ pbmc_container$plots$donor_matrix
 pbmc_container <- plot_donor_matrix(pbmc_container, show_donor_ids = FALSE)
 pbmc_container$plots$donor_matrix
 
+# get significant genes
+pbmc_container <- run_jackstraw(pbmc_container, n_fibers=250, n_iter=500)
+
 # show all loadings plots
 pbmc_container <- get_all_lds_factor_plots(pbmc_container, use_sig_only=FALSE, 
                                            nonsig_to_zero=FALSE,
@@ -90,8 +119,31 @@ pbmc_container <- get_all_lds_factor_plots(pbmc_container, use_sig_only=FALSE,
                                            gene_callouts=FALSE)
 render_all_lds_plots(pbmc_container, n_rows=2)
 
-# get significant genes
-pbmc_container <- run_jackstraw(pbmc_container, n_fibers=100, n_iter=500)
+# show all loadings plots sig genes only
+pbmc_container <- get_all_lds_factor_plots(pbmc_container, use_sig_only=TRUE, 
+                                           nonsig_to_zero=TRUE, sig_thresh=0.01,
+                                           display_genes=FALSE,
+                                           gene_callouts=TRUE)
+render_all_lds_plots(pbmc_container, n_rows=2)
 
 
+
+# plot single loadings plot
+pbmc_container <- plot_loadings_annot(pbmc_container, factor_select=5, use_sig_only=TRUE, nonsig_to_zero=TRUE, annot='none',
+                    pathways=NULL, sim_de_donor_group=NULL, sig_thresh=0.001, display_genes=FALSE, 
+                    gene_callouts=TRUE, callout_n_gene_per_ctype=5, callout_ctypes=NULL, show_xlab=TRUE,
+                    show_var_eplained=TRUE, show_all_legends=TRUE)
+pbmc_container$plots$single_lds_plot
+
+
+
+# get donor sig genes plots
+pbmc_container <- plot_donor_sig_genes(pbmc_container, factor_select=1, 
+                                       top_n_per_ctype=15)
+pbmc_container$plots$donor_sig_genes$Factor1
+
+# get donor sig genes plots
+container <- plot_donor_sig_genes(container, factor_select=1, 
+                                       top_n_per_ctype=15)
+container$plots$donor_sig_genes$Factor1
 
--- ye_pbmc_analysis/lupus_preprocessing.R
+++ ye_pbmc_analysis/lupus_preprocessing.R
@@ -8,10 +8,67 @@ Convert("/home/jmitchel/data/lupus_data/Lupus_study_adjusted.h5ad", dest = "h5se
 # read the h5seurat file
 pbmc <- LoadH5Seurat("/home/jmitchel/data/lupus_data/Lupus_study_adjusted.h5seurat")
 
+# qc analyses
+pbmc[['nCount_RNA']] <- colSums(pbmc[['RNA']]@counts)
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+
+# Visualize QC metrics as a violin plot
+VlnPlot(pbmc, features = c("nCount_RNA", "percent.mt"), ncol = 2)
+
+# remove cells with high mitochondrial expression
+pbmc <- subset(pbmc, subset = percent.mt < 10)
+
 # normalize the data
 pbmc <- NormalizeData(pbmc)
 
-# will use pbmc object directly in lupus_analysis script...
+# limit donors to one sample each
+cells_keep <- c()
+for (d in unique(pbmc@meta.data$Genotype.ID)) {
+  print(d)
+  # subset meta data to a donor
+  meta_sub <- pbmc@meta.data[pbmc@meta.data$Genotype.ID==d,]
+  
+  # get largest donor's batch
+  batch_counts <- table((meta_sub$batch_cov))
+  batch_use <- names(batch_counts)[which(batch_counts==max(batch_counts))]
+  
+  # get cell names corresponding to the donor, batch
+  d_cells <- rownames(meta_sub)[meta_sub$batch_cov==batch_use]
+  cells_keep <- c(cells_keep,d_cells)
+}
+pbmc <- subset(pbmc,cells = cells_keep)
+
+# normalize using trimmed means edgeR method
+samp_ndx <- sample(1:ncol(pbmc[['RNA']]@counts))
+pbmc[['RNA']]@counts <- pbmc[['RNA']]@counts[,samp_ndx]
+pbmc@meta.data <- pbmc@meta.data[samp_ndx,]
+all_nf <- c()
+from_cell <- 1
+to_cell <- 50000
+while (to_cell <= ncol(pbmc[['RNA']]@counts)) {
+  print(from_cell)
+  nf <- calcNormFactors(pbmc[['RNA']]@counts[,from_cell:to_cell])
+  all_nf <- c(all_nf,nf)
+  
+  from_cell <- to_cell + 1
+  if (to_cell== ncol(pbmc[['RNA']]@counts)) {
+    to_cell <- ncol(pbmc[['RNA']]@counts) + 10
+  } else {
+    to_cell <- to_cell + 50000
+    if (to_cell > ncol(pbmc[['RNA']]@counts)) {
+      to_cell <- ncol(pbmc[['RNA']]@counts)
+    }
+  }
+}
+
+# divide by lib size and multiply by scale factor
+lib_sizes <- Matrix::colSums(pbmc[['RNA']]@counts)
+pbmc@assays$RNA <- sweep(pbmc[['RNA']]@counts,MARGIN=2,lib_sizes*all_nf,FUN='/') * 10000
+
+# log transform result
+pbmc@assays$RNA <- log1p(pbmc@assays$RNA)
 
 
 
+# save the cleaned, normalized data
+saveRDS()

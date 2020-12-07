cca55642c25d8fc0d457d16c1a1f44e7cd7bd563
Jonathan <jonathan.mitchel3@gmail.com>
2020-12-07 16:12:05 -0500

    updated lupus preprocessing

--- ye_pbmc_analysis/lupus_preprocessing.R
+++ ye_pbmc_analysis/lupus_preprocessing.R
@@ -18,9 +18,6 @@ VlnPlot(pbmc, features = c("nCount_RNA", "percent.mt"), ncol = 2)
 # remove cells with high mitochondrial expression
 pbmc <- subset(pbmc, subset = percent.mt < 10)
 
-# normalize the data
-pbmc <- NormalizeData(pbmc)
-
 # limit donors to one sample each
 cells_keep <- c()
 for (d in unique(pbmc@meta.data$Genotype.ID)) {
@@ -63,12 +60,36 @@ while (to_cell <= ncol(pbmc[['RNA']]@counts)) {
 
 # divide by lib size and multiply by scale factor
 lib_sizes <- Matrix::colSums(pbmc[['RNA']]@counts)
-pbmc@assays$RNA <- sweep(pbmc[['RNA']]@counts,MARGIN=2,lib_sizes*all_nf,FUN='/') * 10000
+# need to do this in a few steps because the matrix is too large
+norm_mat_all <- log1p(sweep(pbmc[['RNA']]@counts[,1:50000],
+                         MARGIN=2,
+                         lib_sizes[1:50000]*all_nf[1:50000],FUN='/') * 10000)
+from_cell <- 50001
+to_cell <- 100000
+while (to_cell <= ncol(pbmc[['RNA']]@counts)) {
+  print(from_cell)
+  norm_mat <- log1p(sweep(pbmc[['RNA']]@counts[,from_cell:to_cell],
+                           MARGIN=2,
+                           lib_sizes[from_cell:to_cell]*all_nf[from_cell:to_cell],FUN='/') * 10000)
+  norm_mat_all <- cbind(norm_mat_all,norm_mat)
+  
+  from_cell <- to_cell + 1
+  if (to_cell== ncol(pbmc[['RNA']]@counts)) {
+    to_cell <- ncol(pbmc[['RNA']]@counts) + 10
+  } else {
+    to_cell <- to_cell + 50000
+    if (to_cell > ncol(pbmc[['RNA']]@counts)) {
+      to_cell <- ncol(pbmc[['RNA']]@counts)
+    }
+  }
+}
 
-# log transform result
-pbmc@assays$RNA <- log1p(pbmc@assays$RNA)
+# Get regular normalization of the data
+pbmc <- NormalizeData(pbmc)
 
+pbmc@assays$RNA <- pbmc@assays$RNA[,samp_ndx] # because didnt change order yet
 
-
-# save the cleaned, normalized data
-saveRDS()
+# # save the cleaned, normalized data
+# saveRDS(norm_mat_all,file='/home/jmitchel/data/lupus_data/lupus_counts_trim_mean.rds',compress = "xz")
+# saveRDS(pbmc@meta.data,file='/home/jmitchel/data/lupus_data/lupus_meta_trim_mean.rds',compress = "xz")
+# saveRDS(pbmc@assays$RNA,file='/home/jmitchel/data/lupus_data/lupus_counts_regular_norm.rds',compress = "xz")

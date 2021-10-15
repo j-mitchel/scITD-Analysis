6cbb1cfea6982497b8daf58b8bd907676fcef7c3
Jonathan <jonathan.mitchel3@gmail.com>
2021-10-15 13:45:40 -0400

    fig 1 generation

--- figure_generation/figure_1_code.R
+++ figure_generation/figure_1_code.R
@@ -1,173 +1,76 @@
-
+library(scITD)
 library(ComplexHeatmap)
+library(ggplot2)
+library(Seurat)
+library(circlize)
 
-# loading up the DE demuxlet data
-load("/home/jmitchel/data/lupus_data/cell.type.diffexp.RData")
+##### plot the umap with just the two cell types
 
-# get union of significant DE genes from any cell type
-pbmc.expressed.res.sig <- rownames(pbmc.expressed.res)[pbmc.expressed.res$padj<.05]
-nk.expressed.res.sig <- rownames(nk.expressed.res)[nk.expressed.res$padj<.05]
-ncmono.expressed.res.sig <- rownames(ncmono.expressed.res)[ncmono.expressed.res$padj<.05]
-cd8.expressed.res.sig <- rownames(cd8.expressed.res)[cd8.expressed.res$padj<.05]
-cd4.expressed.res.sig <- rownames(cd4.expressed.res)[cd4.expressed.res$padj<.05]
-B.expressed.res.sig <- rownames(cd19.expressed.res)[cd19.expressed.res$padj<.05]
-cM.expressed.res.sig <- rownames(cd14.expressed.res)[cd14.expressed.res$padj<.05]
-dc.expressed.res.sig <- rownames(dc.expressed.res)[dc.expressed.res$padj<.05]
-
-# get genes in all comparisons
-de_list <- list(pbmc.expressed.res, nk.expressed.res, ncmono.expressed.res,
-                cd8.expressed.res, cd4.expressed.res, cd19.expressed.res,
-                cd14.expressed.res, dc.expressed.res)
-
-# get intersection of genes in present in all
-gene_intersect <- rownames(de_list[[1]])
-for (i in 2:length(de_list)) {
-  gene_intersect <- intersect(gene_intersect,rownames(de_list[[i]]))
-}
-
-
-# now get union of DE genes from any cell type that are in gene_intersect
-de_union <- unique(c(pbmc.expressed.res.sig,
-         nk.expressed.res.sig,
-         ncmono.expressed.res.sig,
-         cd8.expressed.res.sig,
-         cd4.expressed.res.sig,
-         B.expressed.res.sig,
-         cM.expressed.res.sig,
-         dc.expressed.res.sig))
-
-de_union_in_all <- de_union[de_union %in% gene_intersect]
-
-tmp <- as.data.frame(cbind(
-  de_list[[1]][de_union_in_all,'log2FoldChange'],
-  de_list[[2]][de_union_in_all,'log2FoldChange'],
-  de_list[[3]][de_union_in_all,'log2FoldChange'],
-  de_list[[4]][de_union_in_all,'log2FoldChange'],
-  de_list[[5]][de_union_in_all,'log2FoldChange'],
-  de_list[[6]][de_union_in_all,'log2FoldChange'],
-  de_list[[7]][de_union_in_all,'log2FoldChange'],
-  de_list[[8]][de_union_in_all,'log2FoldChange']
-))
-
-colnames(tmp) <- c('pbmc', 'nk', 'ncmono',
-                   'cd8', 'cd4', 'B',
-                   'cM', 'dc')
-
-
-
-
-
-
-
-
-
-
-###### for doing just t4 and cM
-# get genes in all comparisons
-de_list <- list(cd4.expressed.res,
-                cd14.expressed.res)
-
-# get intersection of genes in present in all
-gene_intersect <- intersect(rownames(de_list[[1]]),rownames(de_list[[2]]))
-
-# now get union of DE genes from any cell type that are in gene_intersect
-de_union <- unique(c(cd4.expressed.res.sig,
-                     cM.expressed.res.sig))
-
-de_union_in_all <- de_union[de_union %in% gene_intersect]
-
-tmp <- as.data.frame(cbind(
-  de_list[[1]][de_union_in_all,'log2FoldChange'],
-  de_list[[2]][de_union_in_all,'log2FoldChange']
-))
-
-colnames(tmp) <- c('cd4',
-                   'cM')
-
-Heatmap(as.matrix(tmp))
-
-
-
-## should only show color for those genes that are significant! showing
-# the logFC for all genes makes it look like there is more similarity than there actually is
-rownames(tmp) <- de_union_in_all
-
-genes_not_sig_t4 <- !(de_union_in_all %in% cd4.expressed.res.sig)
-tmp[genes_not_sig_t4,'cd4'] <- 0
-
-genes_not_sig_cM <- !(de_union_in_all %in% cM.expressed.res.sig)
-tmp[genes_not_sig_cM,'cM'] <- 0
-
-# order rows as down in both, up in both, down cM only, up cM only, down T4 only, up T4 only
-
-down_both <- rownames(tmp)[rowSums(tmp < 0) == 2]
-up_both <- rownames(tmp)[rowSums(tmp > 0) == 2]
-
-down_cM <- rownames(tmp)[tmp[,'cM']<0]
-down_t4 <- rownames(tmp)[tmp[,'cd4']<0]
-up_cM <- rownames(tmp)[tmp[,'cM']>0]
-up_t4 <- rownames(tmp)[tmp[,'cd4']>0]
-
-down_cM_only <- down_cM[!(down_cM %in% down_t4)]
-down_cM_only <- down_cM_only[!(down_cM_only %in% up_t4)]
-
-up_cM_only <- up_cM[!(up_cM %in% down_t4)]
-up_cM_only <- up_cM_only[!(up_cM_only %in% up_t4)]
-
-down_t4_only <- down_t4[!(down_t4 %in% down_cM)]
-down_t4_only <- down_t4_only[!(down_t4_only %in% up_cM)]
-
-up_t4_only <- up_t4[!(up_t4 %in% down_cM)]
-up_t4_only <- up_t4_only[!(up_t4_only %in% up_cM)]
-
-new_row_order <- c(down_both,up_both,down_cM_only,up_cM_only,down_t4_only,up_t4_only)
-
-# add the rest of the genes
-new_row_order <- c(new_row_order,rownames(tmp)[!(rownames(tmp) %in% new_row_order)])
-
-tmp <- tmp[new_row_order,]
-
-tmp <- as.matrix(tmp)
-colnames(tmp) <- c("CD4+ T","cMonocytes")
-myhmap <- Heatmap(tmp,
-                  name = 'log2FC',
-                  show_row_names=FALSE,
-                  show_row_dend=FALSE,
-                  show_column_dend=FALSE,
-                  cluster_rows=FALSE,
-                  cluster_columns=FALSE,
-                  column_title = "Stim vs Unstim",
-                  column_title_side = "top",
-                  column_title_gp = gpar(fontsize = 20),
-                  border = TRUE)
-
-pdf(file = "/home/jmitchel/figures/for_paper/cd4_cM_stim_DEG.pdf", useDingbats = FALSE,
-    width = 5, height = 7)
-myhmap
-dev.off()
-
-# plot the umap with just these two cell types
+# data from: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE96583 batch2
 embed <- read.table("/home/jmitchel/data/lupus_data/demuxlet_tsne.csv",
                     row.names = 1, sep=',',header = TRUE)
 
 embed <- embed[embed$multiplets=='singlet',]
-dim(embed)
 embed <- embed[embed$cell %in% c("CD4 T cells","CD14+ Monocytes"),]
 
 # create embedding plot
-library(ggplot2)
-ggplot(embed,aes(x=tsne1,y=tsne2,color=cell)) +
-  geom_point()
-
 myembed <- ggplot(embed,aes(x=tsne1,y=tsne2,color=stim)) +
   geom_point() +
   labs(color = "Group") +
   theme_bw()
 
-pdf(file = "/home/jmitchel/figures/for_paper/cd4_cM_stim_embed.pdf", useDingbats = FALSE,
-    width = 6, height = 4)
+# pdf(file = "/home/jmitchel/figures/for_paper/cd4_cM_stim_embed.pdf", useDingbats = FALSE,
+#     width = 6, height = 4)
 myembed
-dev.off()
+# dev.off()
+
+rm(list=ls())
+
+
+
+
+
+
+##### prep counts data for additional analyses
+# same embedding loaded as above
+embed <- read.table("/home/jmitchel/data/lupus_data/demuxlet_tsne.csv",
+                    row.names = 1, sep=',',header = TRUE)
+
+# limit embed to just singlets
+embed <- embed[embed$multiplets=='singlet',]
+
+# data from: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE96583
+pbmc_stim <- Read10X(data.dir = "/home/jmitchel/data/demuxlet/stim")
+pbmc_ctrl <- Read10X(data.dir = "/home/jmitchel/data/demuxlet/ctrl")
+
+# rename barcodes for duplicates between stim and ctrl so that it matches the tsne file
+bcode_dup_mask <- colnames(pbmc_stim) %in% colnames(pbmc_ctrl)
+colnames(pbmc_stim)[bcode_dup_mask] <- sapply(colnames(pbmc_stim)[bcode_dup_mask], function(x) {
+  paste0(x,'1')
+})
+
+# limit each counts matrix to only singlets kept in embed
+stim_keep <- colnames(pbmc_stim) %in% rownames(embed)
+pbmc_stim <- pbmc_stim[,stim_keep]
+
+ctrl_keep <- colnames(pbmc_ctrl) %in% rownames(embed)
+pbmc_ctrl <- pbmc_ctrl[,ctrl_keep]
+
+
+# combine counts matrices
+pbmc_all <- cbind(pbmc_ctrl,pbmc_stim)
+
+pbmc_meta <- embed
+colnames(pbmc_meta)[c(3,6)] <- c('donors','ctypes')
+pbmc_meta$ctypes <- as.factor(pbmc_meta$ctypes)
+pbmc_meta$donors <- as.factor(pbmc_meta$donors)
+pbmc_meta$stim <- as.factor(pbmc_meta$stim)
+
+# need to make each donor + stim combination a separate "donor"
+pbmc_meta$donors <- sapply(1:nrow(pbmc_meta), function(i) {
+  paste0(pbmc_meta[i,'donors'],"_",pbmc_meta[i,'stim'])
+})
+pbmc_meta$donors <- as.factor(pbmc_meta$donors)
 
 
 
@@ -177,17 +80,12 @@ dev.off()
 
 
 
-### now creating a heatmap showing pseudobulked expression for each donor 
-# in each cell type and in each condition
 
-# first get genes tested for DE in both ctypes
+##### generate DE heatmap
+
+# data from: https://github.com/yelabucsf/demuxlet_paper_code/tree/master/s15_interferon_comparison
 load("/home/jmitchel/data/lupus_data/cell.type.diffexp.RData")
 
-cd4.expressed.res_old <- cd4.expressed.res
-cd14.expressed.res_old <- cd14.expressed.res
-
-# cd4.expressed.res.sig <- rownames(cd4.expressed.res)[cd4.expressed.res$padj<.01]
-# cM.expressed.res.sig <- rownames(cd14.expressed.res)[cd14.expressed.res$padj<.01]
 cd4.expressed.res.sig <- rownames(cd4.expressed.res)[cd4.expressed.res$padj<.01]
 cM.expressed.res.sig <- rownames(cd14.expressed.res)[cd14.expressed.res$padj<.01]
 de_list <- list(cd4.expressed.res,
@@ -195,7 +93,6 @@ de_list <- list(cd4.expressed.res,
 
 # get intersection of genes in present in all
 gene_intersect <- intersect(rownames(de_list[[1]]),rownames(de_list[[2]]))
-
 
 # now get union of DE genes from any cell type that are in gene_intersect
 de_union <- unique(c(cd4.expressed.res.sig,
@@ -208,8 +105,6 @@ de_union_in_all <- de_list[[1]][de_union_in_all,'featureData.symbol']
 de_union_in_all <- as.character(de_union_in_all)
 
 # get pseudobulk data 
-load("/home/jmitchel/data/lupus_data/demuxlet_scITD_comparison.RData") # from demuxlet_analysis.R
-
 param_list <- initialize_params(ctypes_use = c("CD14+ Monocytes",
                                                "CD4 T cells"),
                                 ncores = 30, rand_seed = 10)
@@ -218,14 +113,10 @@ pbmc_container <- make_new_container(count_data=pbmc_all, meta_data=pbmc_meta,
                                      params=param_list,
                                      label_donor_sex = FALSE)
 
-pbmc_container <- form_tensor(pbmc_container, donor_min_cells=5, gene_min_cells=10,
+pbmc_container <- form_tensor(pbmc_container, donor_min_cells=5,
                               norm_method='trim', scale_factor=10000,
                               vargenes_method='norm_var_pvals', vargenes_thresh=1,
                               scale_var = TRUE, var_scale_power = .85)
-pbmc_container <- form_tensor(pbmc_container, donor_min_cells=5, gene_min_cells=10,
-                              norm_method='trim', scale_factor=10000,
-                              vargenes_method='norm_var_pvals', vargenes_thresh=1,
-                              scale_var = TRUE, var_scale_power = .5) #testing this with new method
 
 pb_cM <- pbmc_container[["scMinimal_ctype"]][["CD14+ Monocytes"]][["pseudobulk"]]
 pb_cd4 <- pbmc_container[["scMinimal_ctype"]][["CD4 T cells"]][["pseudobulk"]]
@@ -244,15 +135,8 @@ rownames(pb_cd4) <- sapply(rownames(pb_cd4),function(x) {
 })
 names(rownames(pb_cd4)) <- NULL
 
-# # try scaling data before combining
-# pb_cM <- scale(pb_cM)
-# pb_cd4 <- scale(pb_cd4)
-
 # stack dataframes
 pb_total <- rbind(pb_cM,pb_cd4)
-
-# # scale genes to unit variance
-# pb_total <- scale(pb_total)
 
 pb_total <- t(pb_total)
 
@@ -264,12 +148,6 @@ pb_total <- pb_total[de_union_in_all,]
 
 
 ## order rows as down in both, up in both, down cM only, up cM only, down T4 only, up T4 only
-# genes_plot <- rownames(pb_total)
-
-# convert gene symbol names within ct de genes
-cd4.expressed.res <- cd4.expressed.res_old
-cd14.expressed.res <- cd14.expressed.res_old
-
 cd4.sig <- cd4.expressed.res[cd4.expressed.res.sig,]
 cd4.sig.up <- cd4.sig[cd4.sig$log2FoldChange>0,]
 cd4.sig.up.genes <- as.character(cd4.sig.up$featureData.symbol)
@@ -306,9 +184,7 @@ myannot <- rowAnnotation(callouts = anno_mark(at = ndx, which='row',
 # make heatmap
 ta = HeatmapAnnotation(foo = anno_block(gp = gpar(fill = 2:3), labels = c('cMonocytes','CD4+ T')))
 color_lim <- stats::quantile(as.matrix(abs(pb_total)), c(.95))
-# color_lim <- max(pb_total)
 col_fun = colorRamp2(c(-color_lim, 0, color_lim), c("blue", "white", "red"))
-# col_fun = colorRamp2(c(0, color_lim), c("white", "red"))
 myhmap <- Heatmap(pb_total,
                   name = 'expression',
                   show_row_names=FALSE,
@@ -326,12 +202,15 @@ myhmap <- Heatmap(pb_total,
                   col = col_fun,
                   right_annotation = myannot)
 
-pdf(file = "/home/jmitchel/figures/for_paper/demuxlet_de_by_donors_v2.pdf", useDingbats = FALSE,
-    width = 4.75, height = 5.5)
-pdf(file = "/home/jmitchel/figures/for_paper_v2/demuxlet_de_hmap.pdf", useDingbats = FALSE,
-    width = 4.75, height = 5.5)
+# pdf(file = "/home/jmitchel/figures/for_paper_v2/demuxlet_de_hmap.pdf", useDingbats = FALSE,
+#     width = 4.75, height = 5.5)
 myhmap
-dev.off()
+# dev.off()
+
+
+
+
+
 
 
 
@@ -395,6 +274,149 @@ draw(pbmc_container[["plots"]][["all_lds_plots"]][["1"]],
      legend_grouping = "original",
      newpage=TRUE)
 # dev.off()
+
+
+
+
+
+
+
+##### calculating correlation between fold change and loadings for all gene_ctype combos present in both
+tmp_casted_num <- get_one_factor(pbmc_container,1)[[2]]
+
+# get matrix of gene-association p-values
+sig_vectors <- get_significance_vectors(pbmc_container,
+                                        factor_select=1, colnames(tmp_casted_num))
+# convert list to df
+sig_df <- t(as.data.frame(do.call(rbind, sig_vectors)))
+
+# order df same way as in tmp_casted_num
+sig_df <- sig_df[rownames(tmp_casted_num),colnames(tmp_casted_num)]
+##
+
+# remove duplicate gene in cd4.expressed.res
+ndx_check <- which(duplicated(cd4.expressed.res$featureData.symbol))
+ndx_check <- which(cd4.expressed.res$featureData.symbol=='NAA60')
+cd4.expressed.res <- cd4.expressed.res[-ndx_check[2],]
+
+# set rownames to be gene names, not ensemble symbols
+rownames(cd4.expressed.res) <- cd4.expressed.res$featureData.symbol
+rownames(cd14.expressed.res) <- cd14.expressed.res$featureData.symbol
+
+lds_both <- c()
+fc_both <- c()
+sig_both <- c()
+for (i in 1:nrow(tmp_casted_num)) {
+  gene <- rownames(tmp_casted_num)[i]
+  for (j in 1:ncol(tmp_casted_num)) {
+    ctype <- colnames(tmp_casted_num)[j]
+    if (ctype=='CD4 T cells') {
+      if (gene %in% rownames(cd4.expressed.res)) {
+        demux_fc <- cd4.expressed.res[gene,'log2FoldChange']
+        fc_both <- c(fc_both,demux_fc)
+        lds_both <- c(lds_both,tmp_casted_num[i,j])
+        sig_both <- c(sig_both,sig_df[i,j])
+      }
+    } else if (ctype=='CD14+ Monocytes') {
+      if (gene %in% rownames(cd14.expressed.res)) {
+        demux_fc <- cd14.expressed.res[gene,'log2FoldChange']
+        fc_both <- c(fc_both,demux_fc)
+        lds_both <- c(lds_both,tmp_casted_num[i,j])
+        sig_both <- c(sig_both,sig_df[i,j])
+      }
+    }
+  }
+}
+
+cor(lds_both,fc_both,method = 'spearman')
+tmp <- as.data.frame(cbind(lds_both,fc_both,sig_both))
+colnames(tmp) <- c('loading','log2FC','sig_val')
+tmp$sig_val <- tmp$sig_val<.01
+
+# pdf(file = "/home/jmitchel/figures/for_paper_v2/demuxlet_loading_vs_log2FC_v2.pdf", useDingbats = FALSE,
+#     width = 4, height = 1.5)
+ggplot(tmp,aes(x=log2FC,y=loading,color=sig_val)) +
+  geom_point(alpha = .1, pch=19) +
+  theme_bw() +
+  scale_color_manual(values=c("#000000", "#FF0000")) +
+  guides(color = 'none')
+dev.off()
+
+
+
+
+
+
+
+
+
+##### calculating correlation of between scITD pvalues and DE pvalues
+lds_both <- c()
+fc_both <- c()
+gene_ct <- c()
+for (i in 1:nrow(tmp_casted_num)) {
+  gene <- rownames(tmp_casted_num)[i]
+  for (j in 1:ncol(tmp_casted_num)) {
+    ctype <- colnames(tmp_casted_num)[j]
+    if (ctype=='CD4 T cells') {
+      if (gene %in% rownames(cd4.expressed.res)) {
+        demux_fc <- cd4.expressed.res[gene,'padj']
+        fc_both <- c(fc_both,demux_fc)
+        lds_both <- c(lds_both,sig_df[i,j])
+        gene_ct <- c(gene_ct,paste0(gene,"_",ctype))
+      }
+    } else if (ctype=='CD14+ Monocytes') {
+      if (gene %in% rownames(cd14.expressed.res)) {
+        demux_fc <- cd14.expressed.res[gene,'padj']
+        fc_both <- c(fc_both,demux_fc)
+        lds_both <- c(lds_both,sig_df[i,j])
+        gene_ct <- c(gene_ct,paste0(gene,"_",ctype))
+      }
+    }
+  }
+}
+
+tmp <- as.data.frame(cbind(lds_both,fc_both))
+colnames(tmp) <- c('scITD_padj','DE_padj')
+rownames(tmp) <- gene_ct
+tmp$scITD_padj[tmp$scITD_padj<.0001] <- .0001
+tmp$DE_padj[tmp$DE_padj<.000000000000001] <- .000000000000001
+
+# pdf(file = "/home/jmitchel/figures/for_paper_v2/demuxlet_lm_vs_DEpval_v2.pdf", useDingbats = FALSE,
+#     width = 4.5, height = 3)
+ggplot(tmp,aes(x=-log(DE_padj,base=10),y=-log(scITD_padj,base=10))) +
+  geom_point(alpha = 0.3,pch=19) +
+  theme_bw() +
+  geom_hline(yintercept=-log10(.05), linetype="dashed", color = "red") +
+  geom_vline(xintercept=-log10(.05), linetype="dashed", color = "red") +
+  xlab('DE -log10(adj p-value)') +
+  ylab('F1 gene significance\n-log10(adj p-value)')
+# dev.off()
+
+# calculate number of genes above horizontal line and below vertical line
+mask <- tmp
+mask <- -log10(mask)
+pv_thresh=.05
+sum(mask$DE_padj<(-log10(pv_thresh)) & mask$scITD_padj>(-log10(pv_thresh)))
+
+# now get vice versa
+sum(mask$DE_padj>(-log10(pv_thresh)) & mask$scITD_padj<(-log10(pv_thresh)))
+
+# get DE both
+sum(mask$DE_padj>(-log10(pv_thresh)) & mask$scITD_padj>(-log10(pv_thresh)))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
